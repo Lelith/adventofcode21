5d2dc97779f9ee33a7c38dbb074baff958f9e933
Kathrin <kathrin.holzmann@contentful.com>
2021-11-30 17:58:03 +0100

    prepare for day1

---
+++ utils.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+
+module.exports = {
+  trimData: data => data.map(item => item.trim()),
+  calcManhattandistance: (a, b) =>
+    Math.abs(a[0]) - Math.abs(b[0]) + (Math.abs(a[1]) - Math.abs(b[1])),
+  modDataNewline: data => {
+    const formatData = data.split("\n").map(item => parseInt(item.trim(), 10));
+    return formatData;
+  },
+  modDataNewlineStr: data => {
+    const formatData = data.split("\n").map(item => item.trim());
+    return formatData;
+  },
+  modDataBlanklines: data => {
+    const formatData = data.split(/\n{2,}/g);
+    return formatData;
+  },
+  modDataCommas: data => {
+    const formatData = data.split(",").map(item => parseInt(item.trim(), 10));
+    return formatData;
+  },
+  readInput: path => {
+    try {
+      const data = fs.readFileSync(path, "utf8");
+      return data;
+    } catch (e) {
+      console.log("Error:", e.stack);
+    }
+    return false;
+  }
+};
